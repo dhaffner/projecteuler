95dee988c3afc8062b904c45080d37fff22c32ab
dhaffner <dhaffner>
2010-06-25 10:46:50 -0700

    fix whitespace in problem 67 solution

--- problem67/solution.py
+++ problem67/solution.py
@@ -7,12 +7,12 @@ containing a triangle with one-hundred rows.
 
 def run(input='triangle.txt'):
 	lines = map(lambda s: s.rstrip().split(' '), open(input).readlines())
-	
+
 	triangle = lambda i, j: int(lines[i][j])
 	# i = row, j = column
 	# From (i, j) in the triangle, you can "move" to (i+1, j) or (i+1, j+1),
 	# So if maxpath(i, j) is the max path starting at position (i, j), then
-	# maxpath(i, j) = triangle[i, j] + max(maxpath(i+1, j), maxpath(i+1, j+1))	
+	# maxpath(i, j) = triangle[i, j] + max(maxpath(i+1, j), maxpath(i+1, j+1))
 	C = dict()
 	def maxpath(i, j):
 		if (i, j) in C:
@@ -21,10 +21,10 @@ def run(input='triangle.txt'):
 			return 0
 		else:
 			C[(i, j)] = triangle(i, j) + max(maxpath(i + 1, j), \
-											 maxpath(i + 1, j + 1))
+                                             maxpath(i + 1, j + 1))
 		return C[(i, j)]
-			
+
 	print maxpath(0, 0)
 
 if __name__ == '__main__':
-	srun()
+	run()
